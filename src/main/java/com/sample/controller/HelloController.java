2b53ace189f1d0ed48ed2e9f36c946f9f0f1665e
Kuldeep Singh <kuldeep.singh2@hpe.com>
2017-04-08 18:29:07 +0530

    Adding request mapping for home

--- src/main/java/com/sample/controller/HelloController.java
+++ src/main/java/com/sample/controller/HelloController.java
@@ -6,6 +6,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
+	@RequestMapping("/")
+	public String home(){
+		return "This is homepage!";
+	}
+	
 	@RequestMapping("/hello")
 	public String sayHello(){
 		return "Hello from Heroku!";
